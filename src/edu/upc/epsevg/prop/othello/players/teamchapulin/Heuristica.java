4b065598e6bc60c4a5070c003aa91e118a688688
AJBV <antony.joel.bano@estudiantat.upc.edu>
2022-12-24 22:10:09 +0100

    othello final

--- src/edu/upc/epsevg/prop/othello/players/teamchapulin/Heuristica.java
+++ src/edu/upc/epsevg/prop/othello/players/teamchapulin/Heuristica.java
@@ -5,85 +5,196 @@
 package edu.upc.epsevg.prop.othello.players.teamchapulin;
 
 import edu.upc.epsevg.prop.othello.CellType;
-import static edu.upc.epsevg.prop.othello.CellType.opposite;
 import edu.upc.epsevg.prop.othello.GameStatus;
 
-
 /**
- *
- * @author maic1
+ * Clase Heuristica.
+ * 
+ * @author Antony Baño (antony.joel.bano@estudiantat.upc.edu)
+ * @author Michael Zerpa (michael.alberto.zerpa@estudiantat.upc.edu)
  */
 public class Heuristica {
-    public static double evaluador(GameStatus s){
-        double resultat;
-        resultat = 25*CoinParity(s) + 100*CornerCaptured(s); 
-        return resultat * (Stability(s) + 5*Mobility(s));
+    //Per guadar el jugador
+    private static CellType jugador = CellType.EMPTY;
+    //Per guadar el contrari
+    private static CellType adversari = CellType.EMPTY;
+    
+    //Per fer les esquines adjacents positives 
+    private static int X1 = 1, X2 = 1, X3 = 1, X4 = 1;
+    //Matriu que descriu el pes en les diferents posicions en el tauler
+    static int[][] stability_board = new int[][]{ //Tauler 8x8
+        { 100, -20*X1,  10,  5,  5,  10, -20*X2,  100 },
+        { -20*X1, -50,  -2, -2, -2,  -2, -50,  -20*X2 },
+        {  10,  -2,  -1, -1, -1,  -1,  -2,   10 },
+        {   5,  -2,  -1, -1, -1,  -1,  -2,    5 },
+        {   5,  -2,  -1, -1, -1,  -1,  -2,    5 },
+        {  10,  -2,  -1, -1, -1,  -1,  -2,   10 },
+        { -20*X3, -50,  -2, -2, -2,  -2, -50,  -20*X4 },
+        { 100, -20*X3,  10,  5,  5,  10, -20*X4,  100 } 
+    };
+    
+    /**
+     * Funció d'avaluació utilitzat en MiniMax limitat en profunditat i MiniMax amb Iterative Deepeing per calcular el valor d'utilitat que utilitzarà 
+     * algorisme per determinar el següent millor moviment possible.
+     * 
+     * Recull una serie de valors de diferents funcions per tornar el valor heurístic corresponent, el resultat de cada funció se li assigna un pes.
+     * Tenim la funció CoinParity que té assignat un pes de 25, la funció Mobility té un pes de 10,
+     * la funció CornerCaptured té un pes de 100 i la funció Stability no té perque l'agafa de la matriu estatica.
+     * 
+     * @param gs Tauler i estat actual de joc.
+     * 
+     * @return El valor heuristic calculat a partir de la suma de les funcions descrites.
+     */
+    public static int evaluador(GameStatus gs){
+        //Jugador actual 
+        jugador = gs.getCurrentPlayer();
+        //Jugador contrari
+        adversari = CellType.opposite(jugador);
+        
+        //retornar la suma de les diferents funcions descrites.
+        return 25*CoinParity(gs) + 100*CornerCaptured(gs) + (Stability(gs) + 10*Mobility(gs));
     }
     
-
-    // Valor heurístico de paridad de moneda =
-	//100 * (Piezas jugador1 - Piezas jugador2) / (Piezas jugador1 + Piezas jugador2)
-    public static double CoinParity(GameStatus s){
-        CellType jugador = s.getCurrentPlayer();
-        CellType adversari = opposite(jugador);
-        return 100 * ( ( s.getScore(jugador) - s.getScore(adversari) ) / ( s.getScore(jugador) + s.getScore(adversari) ) );
+    /**
+     * Maximitzar la quantitat de fitxes del jugador a partir de la diferència relativa 
+     * A partir del estat actual del tauler, calcular el total de fitxes colocades per part de cada jugador per 
+     * després fer la diferència relativa. 
+     * 
+     * @param gs Tauler i estat actual de joc.
+     * @return Diferència relativa del total fitxes de cada jugador, el valor retornat serà entre -100 i 100
+     */
+    public static int CoinParity(GameStatus gs){
+        //Total de fitxes del jugador
+        int coinsJugador1 = gs.getScore(jugador);
+        //Total de fitxes del contrari
+        int coinsJugador2 = gs.getScore(adversari);
+        int coinsParity = 0;
+        //Jugador actual 
+        if(CellType.PLAYER1 == jugador)
+            //Diferència relativa de les fitxes de cada jugador * 100
+            coinsParity = 100*((coinsJugador1-coinsJugador2)/(coinsJugador1+coinsJugador2));
+        //Jugador contrari
+        if(CellType.PLAYER2 == jugador)
+            //Diferència relativa de les fitxes de cada jugador * 100
+            coinsParity = 100*((coinsJugador2-coinsJugador1)/(coinsJugador2+coinsJugador1));
+        
+        return coinsParity;
     }
     
-    private static double Mobility(GameStatus s) {
+    /**
+     * Maximitzar la quantitat de fitxes del jugador 
+     * A partir del estat actual del tauler, calcular el total de fitxes colocades per part de cada jugador per 
+     * després fer la diferència. 
+     * 
+     * @param gs Tauler i estat actual de joc.
+     * @return Diferència de les fitxes del jugador i el contrari
+     */
+    public static int CoinsCount(GameStatus gs){
+        //Total de fitxes del jugador
+        int coinsJugador1 = gs.getScore(jugador);
+        //Total de fitxes del contrari
+        int coinsJugador2 = gs.getScore(adversari);
+        
+        int coinsCount = 0;
+        
+        //Jugador actual 
+        if(CellType.PLAYER1 == jugador)
+            coinsCount = coinsJugador1-coinsJugador2;
+        //Jugador contrari
+        if(CellType.PLAYER2 == jugador)
+            coinsCount = coinsJugador2-coinsJugador1;
+        
+        return coinsCount;
+    }
+    
+    /**
+     * Potenciar la posició en les esquines per al jugador 
+     * A partir del estat actual del tauler, compten les esquines ocupades per 
+     * part de cada jugador, tener les esquines captures permet tenir millor estabilitat
+     * 
+     * @param s Tauler i estat actual de joc.
+     * @return Diferència relativa de les esquines conquistades per part dels jugador, el valor retornat serà entre -100 i 100
+     */
+    private static int CornerCaptured(GameStatus s) {
+        //Comptabilitzar del jugador
+        int j_corners = 0;
+        //Comptabilitzar del contrari
+        int a_corners = 0;
+        
+        // La variable "x" serà utilitzada en la funció Stability en el cas que el jugador 
+        // tingui una fitxa en una esquina per augmentar la captura de les posicions adjacents
+        
+        // Augmentar el comptador de jugador o contrari si tenen la fitxa posada en la esquina (0,0)
+        if(s.getPos(0,0) == jugador){    j_corners++;  X1=-1;}
+        if(s.getPos(0,0) == adversari)   a_corners++;
+        
+        // Augmentar el comptador de jugador o contrari si tenen la fitxa posada en la esquina (0,7)
+        if(s.getPos(0,7) == jugador){   j_corners++;  X2=-1;}
+        if(s.getPos(0,7) == adversari)   a_corners++;
+        
+        // Augmentar el comptador de jugador o contrari si tenen la fitxa posada en la esquina (7,0)
+        if(s.getPos(7,0) == jugador){   j_corners++;  X3=-1;}
+        if(s.getPos(7,0) == adversari)   a_corners++;
+        
+        // Augmentar el comptador de jugador o contrari si tenen la fitxa posada en la esquina (7,7)
+        if(s.getPos(7,7) == jugador){   j_corners++;  X4=-1;}
+        if(s.getPos(7,7) == adversari)   a_corners++;
+        
+        //Fer la diferència relativa de les esquines captures si ha ha alguna esquina capturada
+        if(j_corners + a_corners != 0)
+            return 100 * ( (j_corners-a_corners) / (j_corners + a_corners) );
+        //Si ningú jugador té una esquina capturada, retornar un 0
+        else    return 0;
+    }
+    
+    /**
+     * Augmentar la quantitat de moviments del jugador i disminuir la movilitat del contrari 
+     * A partir del estat actual del tauler, comprovem la la quantitat de moviments disponibles de cada jugador, 
+     * per després fer la diferència relativa a partir dels moviments calculats, potenciem el resultat si el jugador té alguna esquina capturada,
+     * en cas que l'adversari tingi més esquines capturares fem més baix el valor resultat de diferència relativa.
+     *  
+     * @param s Tauler i estat actual de joc.
+     * @return Diferència relativa de les esquines conquistades per part dels jugador
+     */
+    private static int Mobility(GameStatus s) {
+        //Quantitat de moviments que pot fer el jugador en el estat actual del tauler
         int jugador_moves = s.getMoves().size();
         s.skipTurn();
+        //Quantitat de moviments que pot fer el contrari en el estat actual del tauler
         int adversari_moves = s.getMoves().size();
-        
-        if((jugador_moves + adversari_moves) != 0  )
-            return 100 * ( (jugador_moves - adversari_moves) / (jugador_moves + adversari_moves) );
-        else
-            return 0;
-    }
-
-    private static double CornerCaptured(GameStatus s) {
-        int jugador_corners = 0;
-        int adversari_corners = 0;
-        
-        if(s.getPos(0,0)==s.getCurrentPlayer()) jugador_corners++;
-        else{if(s.getPos(0,0)!=CellType.EMPTY)   adversari_corners++;}
-        
-        if(s.getPos(0,7)==s.getCurrentPlayer()) jugador_corners++;
-        else{if(s.getPos(0,7)!=CellType.EMPTY)   adversari_corners++;}
-        
-        if(s.getPos(7,0)==s.getCurrentPlayer()) jugador_corners++;
-        else{if(s.getPos(7,0)!=CellType.EMPTY)   adversari_corners++;}
-        
-        if(s.getPos(7,7)==s.getCurrentPlayer()) jugador_corners++;
-        else{if(s.getPos(7,7)!=CellType.EMPTY)   adversari_corners++;}
-        
-        if((jugador_corners + adversari_corners) != 0)
-            return 100 * ( (jugador_corners - adversari_corners) / (jugador_corners + adversari_corners) );
+        //Fer la diferència relativa si algún jugador te almanys un moviment
+        if(jugador_moves + adversari_moves != 0)
+            return 100*( (jugador_moves - adversari_moves) / (jugador_moves + adversari_moves) );
+        //En cas que ningú juggador tingui moviments, reduir el resultat
         else return 0;
     }
     
+    /**
+     * Potenciar el posicionament del jugador 
+     * A partir de la matriu stability_board que descriu el valors de les posicions del tauler, 
+     * comptabilitzem les posicions de les fitxes del jugador descrites en la matriu, fent el mateix per al contrri.
+     * La matriu té en compta les adjacències de les esquines en cas de ser capturares per part del jugador per maximitzar 
+     * les captures de les poscions adjacents veïnes.
+     * 
+     * @param s Tauler i estat actual de joc.     
+     * @return Retorna la diferència deprés d'haver calculat el pes de les posicions de cada jugador
+     */
     private static int Stability(GameStatus s) {
-        int jugador_stability = 0;
-        int adversari_stability = 0;
+        //Variable per guardar el pes de les posicions del jugador
+        int stability_jugador = 0;
+        //Variable per guardar el pes de les posicions del contrari
+        int stability_adversari = 0;
         
-        int[][] stability_board = new int[][]{ //8x8
-                    { 4, -3,  2,  2,  2,  2, -3,  4},
-                    {-3, -4, -1, -1, -1, -1, -4, -3},
-                    { 2, -1,  1,  0,  0,  1, -1,  2},
-                    { 2, -1,  0,  1,  1,  0, -1,  2},
-                    { 2, -1,  0,  1,  1,  0, -1,  2},
-                    { 2, -1,  1,  0,  0,  1, -1,  2},
-                    {-3, -4, -1, -1, -1, -1, -4, -3},
-                    { 4, -3,  2,  2,  2,  2, -3,  4}
-            };
-        
+        //Recorregut del tauler per fer la suma en funció del pes descrit en la mstriu stability_board
         for(int i = 0; i < s.getSize(); ++i){
             for(int j = 0; j < s.getSize(); ++j){
-                if(s.getPos(i,j) == s.getCurrentPlayer()) jugador_stability+=stability_board[i][j];
-                else{ if(s.getPos(i,j)!=CellType.EMPTY) adversari_stability-=stability_board[i][j];}
+                //Comprovar si el jugador té posada una fitxa en la posició comparada
+                if(s.getPos(i,j) == jugador) stability_jugador+=stability_board[i][j];
+                //Comprovar si el contrari té posada una fitxa en la posició comparada
+                if(s.getPos(i,j) == adversari) stability_adversari+=stability_board[i][j];
             }
         }
         
-        return jugador_stability - adversari_stability;
+        return stability_jugador-stability_adversari;
     }
-    
 }
---
+++ src/edu/upc/epsevg/prop/othello/players/teamchapulin/PlayerID.java
@@ -0,0 +1,355 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.upc.epsevg.prop.othello.players.teamchapulin;
+
+import edu.upc.epsevg.prop.othello.CellType;
+import edu.upc.epsevg.prop.othello.GameStatus;
+import edu.upc.epsevg.prop.othello.IAuto;
+import edu.upc.epsevg.prop.othello.IPlayer;
+import edu.upc.epsevg.prop.othello.Move;
+import edu.upc.epsevg.prop.othello.SearchType;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Jugador MiniMaxID que implementa el mecanisme de timeout.
+ * 
+ * @author Antony Baño (antony.joel.bano@estudiantat.upc.edu)
+ * @author Michael Zerpa (michael.alberto.zerpa@estudiantat.upc.edu)
+ */
+public class PlayerID implements IPlayer, IAuto {
+    
+    private final String nom;
+    private InfoNode[] TT;
+    private long nodesExplorats = 0L;
+    private boolean temps;
+    private final long[][][] zobristKeys;
+    private int depth;
+    
+    /**
+     * Constructor del jugador IA MiniMaxID amb timeout.
+     * 
+     * Es dona per defecte el nom del jugador i es crea la taula de transposicions i 
+     * el vector tridimensional de les claus de Zobrist.
+     * 
+     */
+    public PlayerID() {
+        this.nom = "MiniMaxID";
+        this.TT = new InfoNode[714285714];
+        this.zobristKeys = creaZobristRandom();
+    }
+    
+    /**
+    * Base de dades dels nodes de la taula de transposicio.
+    */
+    public class InfoNode {
+        private final long hashKey;
+        private final int heuristica;
+        private final byte profPendent;
+        private final byte tipus;
+        
+        /**
+         * Constructor d'un InfoNode.
+         * 
+         * Asignem els diferents valors per les variables.
+         * 
+         * @param key clau Zobrist de l'estat del tauler
+         * @param valor valor heuristic del moviment
+         * @param depth profunditat assolida
+         * @param tipus tipus de node 0-> exacte, 1-> poda alpha, 2-> poda beta 
+         */
+        public InfoNode(long key, int valor, byte depth, byte tipus) {
+            this.hashKey = key;
+            this.heuristica = valor;
+            this.profPendent = depth;
+            this.tipus = tipus;
+        }
+    }
+    
+    /**
+     * Decideix el moviment del jugador donat un tauler i un color de peça que
+     * ha de posar.
+     *
+     * @param s Tauler i estat actual de joc.
+     * @return el moviment que fa el jugador.
+     */
+    @Override
+    public Move move(GameStatus s) {
+        //Inicialitzem variables
+        nodesExplorats = 0;depth = 0;int valor = 0;
+        int millorMov = 0, millorMov2 = 0;
+        
+        //Comenza el temps
+        temps = true;
+        
+        //Llista de moviments possibles
+        ArrayList<Point> moves = s.getMoves();
+        
+        //Si el jugador pot tirar explora els diferents moviments
+        if(s.currentPlayerCanMove()){
+            long hashKey;
+            //Mentre no s'acabi el temps, continua
+            while(temps){
+                valor = Integer.MIN_VALUE;
+                depth++;
+                //Ordenem els moviments posant els millors primer
+                moves = ordena(moves, millorMov);
+                //Recorregut dels moviments possibles 
+                for(int i = 0; i < moves.size(); ++i){
+                    //Copia de l'estat del tauler
+                    GameStatus gs_nou = new GameStatus(s);
+                    //Fem un moviment
+                    gs_nou.movePiece(moves.get(i));
+                    //Si es un moviment guanyador, fem return del moviment
+                    if(gs_nou.checkGameOver()){
+                        return new Move(moves.get(i), nodesExplorats, depth, SearchType.MINIMAX_IDS);
+                    }
+                    //Calculem el valor hash zobrist de l'estat actual del tauler
+                    hashKey = calculateHashTable(gs_nou);
+                    //Explorem el moviment del node candidat a millor moviment
+                    int candidat = miniMax(gs_nou,depth,depth,Integer.MIN_VALUE,Integer.MAX_VALUE,false, hashKey);
+                   
+                    //Si el node candidat te el maxim valor el guardem
+                    if(candidat == Integer.MAX_VALUE){
+                        millorMov = i;
+                    }
+                    
+                    //Si el node candidat te el mateix valor heuristic que l'anterior
+                    if(valor == candidat){
+                        int desempat = Heuristica.stability_board[moves.get(i).x][moves.get(i).y];
+                        if(millorMov2 < desempat) millorMov = i;
+                    }
+                    
+                    //Si el node candidat es millor fins ara el guardem
+                    if(valor < candidat){                
+                        valor = candidat;
+                        millorMov = i;
+                        millorMov2 = Heuristica.stability_board[moves.get(i).x][moves.get(i).y];
+                    }
+                    
+                    //Si s'ha acabat el temps acaba el bucle
+                    if(!temps) break;
+                }  
+            }
+        }   else{s.skipTurn();} //El jugador no pot tirar, pasa torn
+        
+        return new Move(moves.get(millorMov), nodesExplorats, depth, SearchType.MINIMAX_IDS);
+    }
+    
+    /**
+     * Genera possibles jugades simulant tirades a cada columna, escollint el millor moviment per a tu, suposant que l'adversari escollirà  el pitjor moviment per a tu. 
+     * 
+     * La poda alpha-beta és una tècnica de cerca que redueix el nombre de nodes avaluats en un arbre de joc per l'algorisme Minimax.
+     * Aquesta cerca alfa-beta actualitza el valor dels paràmetres segons es recorre l'arbre. El mètode ha de fer la poda de 
+     * les branques restants quan el valor actual que s'està examinant sigui pitjor que el valor actual de alpha o beta per a MAX o MIN.
+     * 
+     * @param gs Tauler i estat actual de joc.
+     * @param depth profunditat actual de la tauler
+     * @param depth_a profunditat que es preten assolir
+     * @param alpha valor per maximitxar benefici
+     * @param beta valor per minimitzar perdudes
+     * @param maximitza valor bolea per saber si es maximitza o es minimitza
+     * @param key clau hash de l'estat actual del tauler
+     * @return Eleccio de la jugada amb maxim valor heuristic.
+     */ 
+    private int miniMax(GameStatus gs, int depth, int depth_a, int alpha, int beta, boolean maximitza, long key) {  
+        //Index de l'estat de la taula despres del moviment
+        int estat = (int)(key%TT.length);
+        
+        //Prob-Cut 
+        if(TT[estat] != null && TT[estat].hashKey == key && TT[estat].profPendent >= depth ){
+            switch (TT[estat].tipus) {
+                case 0 -> {//EXACT
+                    return TT[estat].heuristica;
+                }
+                case 1 -> {//ALPHA
+                    alpha = Math.max(alpha, TT[estat].heuristica);
+                }
+                case 2 ->{ //BETA
+                    beta = Math.min(beta, TT[estat].heuristica);
+                }
+            }
+            //Poda alpha-beta
+            if(alpha >= beta)   return TT[estat].heuristica;
+        }
+        
+        //Final de partida
+        if(gs.isGameOver()){
+            return Integer.MAX_VALUE; 
+        }
+        
+        //Profunditat igual a cero
+        if(depth == 0){
+            nodesExplorats++;
+            int valor = (int) Heuristica.evaluador(gs);
+            
+            //Si l'estat no existeix 
+            if(TT[estat] == null){
+                TT[estat] = afegir(key, valor, (byte) depth_a, (byte)0);
+            }
+            //Si l'estat existeix i es el mateix estat o es una col.lisio
+            else if(TT[estat] != null){
+                if(TT[estat].profPendent <= depth_a){
+                    TT[estat] = afegir(key, valor, (byte) depth_a, (byte)0);
+                }
+            }
+            //Retorna el valor heuristic
+            return valor;   
+        }
+        
+        //Llista de moviments possibles
+        ArrayList<Point> moves_minmax = gs.getMoves();
+        long hashKey;
+ 
+        //Maximitza 
+        if (maximitza){       
+            //Recorregut dels moviments possibles 
+            for(int i=0;i<moves_minmax.size();i++){
+                //Copia de l'estat del tauler
+                GameStatus gs_max = new GameStatus(gs);
+                //Fem un moviment
+                gs_max.movePiece(moves_minmax.get(i));
+                //Calculem el valor hash zobrist de l'estat actual del tauler
+                hashKey = calculateHashTable(gs_max);
+                //Si es un moviment guanyador, no fa falta seguint explorant
+                if(gs_max.checkGameOver()){    return Integer.MAX_VALUE;}       
+                //Seguim l'exploracio del node
+                int min = miniMax(gs_max, depth-1, depth_a, alpha, beta, !maximitza, hashKey);
+                //Valor alpha
+                alpha = Math.max(alpha, min);                    
+                //Poda alpha-beta
+                if (beta <= alpha){ 
+                    //Guardem a la taula de transposicio el cut-off de la poda com alpha
+                    if(TT[estat]== null || ((TT[estat].hashKey == key && TT[estat].profPendent <= depth))){
+                        TT[estat] = afegir(key, alpha, (byte) depth, (byte)1);
+                    }   
+                    break;  
+                }
+                
+                //Si s'ha acabat el temps acaba el bucle
+                if(!temps) break;
+            }      
+            return alpha;
+        } 
+        //Minimiza
+        else{
+            //Recorregut dels moviments possibles 
+            for(int i=0;i<moves_minmax.size();i++){
+                //Copia de l'estat del tauler
+                GameStatus gs_min = new GameStatus(gs);
+                //Fem un moviment
+                gs_min.movePiece(moves_minmax.get(i));
+                //Si es un moviment guanyador, no fa falta seguint explorant
+                if(gs_min.checkGameOver()){    return Integer.MIN_VALUE;}
+                //Calculem el valor hash zobrist de l'estat actual del tauler
+                hashKey = calculateHashTable(gs_min);
+                //Seguim l'exploracio del node
+                int max = miniMax(gs_min, depth-1, depth_a, alpha, beta, !maximitza, hashKey);
+                //Valor beta
+                beta = Math.min(beta, max);
+                //Poda alpha-beta
+                if (beta <= alpha){ 
+                    //Guardem a la taula de transposicio el cut-off de la poda com beta
+                    if(TT[estat]== null || ((TT[estat].hashKey == key && TT[estat].profPendent <= depth))){
+                        TT[estat] = afegir(key, beta, (byte) depth, (byte)2);
+                    }
+                    break;  
+                }    
+                    
+                //Si s'ha acabat el temps acaba el bucle
+                if(!temps) break;
+            }
+            return beta;
+        }
+    }
+   
+    /**
+     * Ordena els moviments del node.
+     * 
+     * Intercanvia els moviments, posant els millors moviments dels primers de la llista.
+     * 
+     * @param moves llista de moviments
+     * @param millorMov index del millor moviment a intercanviar
+     * @return una llista nova, amb el millor moviment rebut el primer.
+     */
+    private ArrayList<Point> ordena(ArrayList<Point> moves, int millorMov) {
+        Collections.swap(moves, 0, millorMov);
+        return moves;
+    }
+    
+    /**
+     * Calcula el valor de hash del tauler del joc, fent l'operacio XOR
+     * de les caselles on es troben els dos tipus de jugadors.
+     * 
+     * @param gs Tauler i estat actual de joc.
+     * @return valor hash del tauler de joc.
+     */
+    private long calculateHashTable(GameStatus gs){
+        long key = 0;
+        for (int i = 0; i < gs.getSize(); ++i) {
+            for (int j = 0; j < gs.getSize(); ++j) {
+                if(gs.getPos(i, j) == CellType.PLAYER1) 
+                    key ^= zobristKeys[i][j][0];
+                if(gs.getPos(i, j) == CellType.PLAYER2) 
+                    key ^= zobristKeys[i][j][1];
+            }
+        }
+        return Math.abs(key);
+    }    
+    
+    /**
+     * Genera les claus Zobrist a un vector tridimensional on els indexs son
+     * claus[fila][columa][tipus de peça] i els dona un valor aleatori de 64 bits.
+     * 
+     * @return vector tridimensional de números aleatoris de 64 bits
+     */
+    private long[][][] creaZobristRandom() {
+        Random rand = new Random();
+        long[][][] keys = new long[8][8][2];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                for (int k = 0; k < 2; k++) {
+                    keys[i][j][k] = rand.nextLong();
+                }
+            }
+        }
+        return keys;
+    }
+    
+    /**
+     * Crea un InfoNode pero poder afegir-lo a la taula de transposicio.
+     * 
+     * @param key clau Zobrist de l'estat del tauler
+     * @param valor valor heuristic del moviment
+     * @param depth profunditat assolida
+     * @param tipus tipus de node 0-> exacte, 1-> poda alpha, 2-> poda beta 
+     * @return un nou InfoNode
+     */
+    private InfoNode afegir(long key, int valor, byte depth, byte tipus){       
+        return new InfoNode(key, valor, depth, tipus);                                             
+    }
+    
+    /**
+     * Ens avisa que hem de parar la cerca en curs perquè s'ha exhaurit el temps
+     * de joc.
+     */
+    @Override
+    public void timeout() {
+        temps = false;
+    }
+    
+    /**
+     * Retorna el nom del jugador que s'utlilitza per visualització a la UI
+     *
+     * @return Nom del jugador
+     */
+    @Override
+    public String getName() {
+        return nom;
+    }
+    
+}
